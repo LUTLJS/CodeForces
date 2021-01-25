8d0df2d6b8eee6318d60b3f6d028bc910eb4c5bd
lutljs <lutljs@outlook.com>
2021-01-25 11:16:37 +0800

    modifi AlyonaAndNumbers solution

--- A/AlyonaAndNumbers.java
+++ A/AlyonaAndNumbers.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class AlyonaAndNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt(), count = 0;
+        int n = sc.nextInt(), m = sc.nextInt();
+        long count = 0;
         sc.close();
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
