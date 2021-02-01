054ee8a9e7b6be9331c4e437efe8ed3269e2d3fd
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-01 06:21:01 +0000

    Add files via upload

---
+++ Div2_B/Decoding.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Decoding{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        s.nextLine();
+        String ss=s.nextLine(),s1="";
+        s.close();
+        if(n%2==1){
+            for(int i=n-2;i>0;i-=2)s1+=ss.charAt(i);
+            for(int i=0;i<n;i+=2)s1+=ss.charAt(i);
+        }else{
+            for(int i=n-2;i>1;i-=2)s1+=ss.charAt(i);
+            s1+=ss.substring(0,2);
+            for(int i=3;i<n;i+=2)s1+=ss.charAt(i);
+        } 
+        System.out.print(s1);
+    }
+}
