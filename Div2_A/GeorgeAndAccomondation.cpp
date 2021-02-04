1e67ec9e48e56a1cc2c9dee26ecb19e9c403ccb7
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-04 02:55:46 +0000

    Add files via upload

---
+++ Div2_A/GeorgeAndAccomondation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,p,q,count=0;
+    cin>>n;
+    while(cin>>p){
+        cin>>q;
+        if(q-p>=2)count++;
+    }
+    cout<<count;
+    return 0;
+}
