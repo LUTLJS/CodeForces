1548e443324f5a8f1dc4cb21c5da7b1b8f73f7e5
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-05 11:42:03 +0000

    Update EuclideanGCD.cpp

--- Algebra/EuclideanGCD.cpp
+++ Algebra/EuclideanGCD.cpp
@@ -4,8 +4,12 @@ using namespace std;
 int gcd(int a,int b);
 int main(){
     cout<<gcd(11,17);
+    cout<<lcm(20,25);
     return 0;
 }
 int gcd(int a,int b){
     return b?:gcd(b,a%b):a;
+}
+int lcm(int a,int b){
+    return a/gcd(a,b)*b;
 }
