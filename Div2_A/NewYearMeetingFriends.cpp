f1e9ad037a68d3836456f00ad6b267efb45b885e
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-05 00:34:07 +0000

    Update NewYearMeetingFriends.cpp

--- Div2_A/NewYearMeetingFriends.cpp
+++ Div2_A/NewYearMeetingFriends.cpp
@@ -5,6 +5,6 @@ int main(){
     cin.tie(0);
     int a[3];
     for(int i=0;i<3;i++)cin>>a[i];
-    cout<<max_element(a,a+3)-min_element(a,a+3);
+    cout<<*max_element(a,a+3)-*min_element(a,a+3);
     return 0;
 }
