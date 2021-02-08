7cd7f5f19a9fda6f8cb062a6c11871233d58e736
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-08 06:03:44 +0000

    Create TriangularNumbers_.cpp

---
+++ Div2_A/TriangularNumbers_.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int a=0,m;cin>>m;
+    for(int i=1;i<=33;i++){if(i(i+1)==2*m)a=1;}
+    if(a)cout<<"YES";
+    else cout<<"NO";
+    return 0;
+}
