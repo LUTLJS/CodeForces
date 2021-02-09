f96c76912211810e70225f02dd98c5c95dc12cc8
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-09 00:16:00 +0000

    Add files via upload

---
+++ Div2_A/WizardDuel.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+pair<int,int> p[101];
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int l,p,q;cin>>l>>p>>q;
+    cout<<l/(p+q+.0000)*p;
+    return 0;
+}
