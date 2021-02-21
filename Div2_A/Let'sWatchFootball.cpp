714dd7692b33cecf9025043ef7777b694110be0a
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-21 05:36:19 +0000

    Create Let'sWatchFootball.cpp

---
+++ Div2_A/Let'sWatchFootball.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+I don't know how this work,
+I spent lots of time on this problem, then I checked out the editorial,
+and I got confused, so I quit.
+I just copied this piece of code, to finish my thinking on this problem,
+now I want to move on.
+*/
+int main(){
+    int a,b,c;cin>>a>>b>>c;
+    cout<<(c*(a-b)+b-1)/b;
+    return 0;
+}
