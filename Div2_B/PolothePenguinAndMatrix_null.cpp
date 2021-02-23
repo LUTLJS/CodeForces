ee5d01151a5dab355cdfc03959139354e9c8761e
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-23 06:04:37 +0000

    Create PolothePenguinAndMatrix_null.cpp

---
+++ Div2_B/PolothePenguinAndMatrix_null.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+I don't understand how to solve this.
+After checking out the editorial, I still didn't get it.
+
+*/
+int main(){
+    int n,m,d;cin>>n>>m>>d;
+    long long sum1=0,sum2=n;
+    int x;
+    while(cin>>x)sum1+=x;
+    for(int i=1;;i++){
+        sum2=i*n;
+        if((sum2-sum1)%d)
+    }
+    
+}
