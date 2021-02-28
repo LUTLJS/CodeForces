edd89dcf58faccfb0cddddd68be61fa35450d893
LUTLJS <LUTLJS@OUTLOOK.com>
2021-02-28 11:26:46 +0000

    Update C - Kaprekar Number_2.cpp

--- AtCoder/C - Kaprekar Number_2.cpp
+++ AtCoder/C - Kaprekar Number_2.cpp
@@ -6,13 +6,25 @@ int main(){
   string s=to_string(n),ma,mi;
   for(int i=1;i<=k;i++){
     // Initialize all elements to be 0.
-    for(int m=0;m<10;m++)digits[m]=0;
+    for(int m=0;m<10;m++)
+      digits[m]=0;
     // Calculating how many times each digit appears
-    for(char x:s)digits[x-'0']++;
+    for(char x:s)
+      digits[x-'0']++;
     // Initializing max and min to empty string
     ma="",mi="";
-    for(int j=1;j<10;j++){p=digits[j];if(p!=0)while(p--)mi+=to_string(j);}
-    for(int j=9;j>=0;j--){p=digits[j];if(p!=0)while(p--)ma+=to_string(j);}
+    for(int j=1;j<10;j++){
+      p=digits[j];
+      if(p!=0)
+        while(p--)
+          mi+=to_string(j);
+    }
+    for(int j=9;j>=0;j--){
+      p=digits[j];
+      if(p!=0)
+        while(p--)
+          ma+=to_string(j);
+    }
     s=to_string((ma.length()!=0&&mi.length()!=0)?stoi(ma)-stoi(mi):0);
   }
   cout<<s;
