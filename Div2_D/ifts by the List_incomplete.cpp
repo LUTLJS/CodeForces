6c85d8d26260769e97a9d4a4d3fa0a140053e7ee
LUTLJS <LUTLJS@OUTLOOK.com>
2021-03-22 08:09:35 +0000

    Create ifts by the List_incomplete.cpp

---
+++ Div2_D/ifts by the List_incomplete.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    int men,relations;cin>>men>>relations;
+    for(int i=0;i<relations;i++){
+        int father,son;cin>>father>>son;
+    }
+    map<int,int> towhoms;
+    for(int i=0;i<men;i++){
+        int towhom;cin>>towhom;
+        towhoms[towhom]++;
+    }
+    
+    
+    
+    
+}
