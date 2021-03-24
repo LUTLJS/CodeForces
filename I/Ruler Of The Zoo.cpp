185711f9b801caf937c3606f18f6b0d7521593ae
LUTLJS <LUTLJS@OUTLOOK.com>
2021-03-24 08:08:05 +0000

    Create  Ruler Of The Zoo.cpp

---
+++ I/Ruler Of The Zoo.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Instead of doing these hard problems, I'd rather solve some problems with something I don't know yet.
+This way I can learn something and make it my own.
+Because some experienced people said that these hard problems need generaly abilities. But now I lack some basic knowledge about someting.
+So I'm going to solve problems that require some knowledge I don't know yet. And I can improve from solving them.
+Just like the last one, I learnt something about directed acyclic graph and its topological sorting.
+That's great experience.
+*/
+/*
+Firstly, I thought we should use a linkedlist but didn't know how to determine whether it's going to be infinite loop.
+So, the input is a queue, but now we turn it into a circle, we see it as a circle.They now stand in a circle.
+If the king wins, swap these two element, otherwise don't swap. But, the king always move clockwise.
+*/
+int main(){
+}
