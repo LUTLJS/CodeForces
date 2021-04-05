49829c256dda9baec9fa63bae9faf1bc9fa1ba85
LUTLJS <LUTLJS@OUTLOOK.com>
2021-04-05 15:59:06 +0800

    Create Round Table Knights_null.cpp

---
+++ Div2_C/Round Table Knights_null.cpp
@@ -0,0 +1,10 @@
+/*
+Frezze the length of the side.
+Assume such polygon exists.
+Let this length equals k.
+then k divides n
+We have exactly k of such polygons.
+Each one of them has exactly n/k vertices.
+Check every of the polygons for goodness by review all of its vertices.
+
+*/
